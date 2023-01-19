2c7f8416f2518c1aaa35f152e90200de827b9971
Vanja Oljaca <vanja.oljaca@me.com>
2023-01-19 14:30:46 -0800

    Progress: getting azure functions to work

---
+++ main/index.ts
@@ -0,0 +1,29 @@
+﻿import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import * as df from "durable-functions"
+
+/*
+TODO:
+- run locally
+- deploy from github
+- deploy to azure
+- fix entity storage
+- add tests
+- connect to gpt
+*/
+
+const httpStart: AzureFunction = async function (context: Context, req: HttpRequest) {
+    const client = df.getClient(context);
+    const id: string = req.params.id;
+    const entityId = new df.EntityId("Counter1", id);
+
+    if (req.method === "POST") {
+        // increment value
+        await client.signalEntity(entityId, "add", 1);
+    } else {
+        // reads current state of entity
+        const stateResponse = await client.readEntityState<number>(entityId);
+        return { body: stateResponse.entityState };
+    }
+};
+
+export default httpStart;
